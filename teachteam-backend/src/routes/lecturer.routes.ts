692d6430db03ea4954c18a2f2aabe57cd7bd32f8
Joshua <s4013483@student.rmit.edu.au>
2025-05-05 13:50:16 +1000

    Added lecturer route

---
+++ teachteam-backend/src/routes/lecturer.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { LecturerController } from "../controller/LecturerController";
+
+const router = Router();
+const lecturerController = new LecturerController();
+
+router.get("/lecturer", async (req, res) => {
+  await lecturerController.all(req, res);
+});
+
+router.get("/lecturer/:id", async (req, res) => {
+  await lecturerController.one(req, res);
+});
+
+router.post("/lecturer", async (req, res) => {
+  await lecturerController.save(req, res);
+});
+
+router.put("/lecturer/:id", async (req, res) => {
+  await lecturerController.update(req, res);
+});
+
+router.delete("/lecturer/:id", async (req, res) => {
+  await lecturerController.remove(req, res);
+});
+
+export default router;
