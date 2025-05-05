86942726c959c4dc50465df06d368d13dad0a364
Joshua <s4013483@student.rmit.edu.au>
2025-05-05 14:50:06 +1000

    Fixed tutor routes to route to /tutors

--- teachteam-backend/src/routes/tutor.routes.ts
+++ teachteam-backend/src/routes/tutor.routes.ts
@@ -4,27 +4,27 @@ import { TutorController } from "../controller/TutorController";
 const router = Router();
 const tutorController = new TutorController();
 
-router.get("/lecturers", async (req, res) => {
+router.get("/tutors", async (req, res) => {
   await tutorController.all(req, res);
 });
 
-router.get("/lecturers/:id", async (req, res) => {
+router.get("/tutors/:id", async (req, res) => {
   await tutorController.one(req, res);
 });
 
-router.get("/lecturers/:email", async (req, res) => {
+router.get("/tutors/:email", async (req, res) => {
   await tutorController.email(req, res);
 });
 
-router.post("/lecturers", async (req, res) => {
+router.post("/tutors", async (req, res) => {
   await tutorController.save(req, res);
 });
 
-router.put("/lecturers/:id", async (req, res) => {
+router.put("/tutors/:id", async (req, res) => {
   await tutorController.update(req, res);
 });
 
-router.delete("/lecturers/:id", async (req, res) => {
+router.delete("/tutors/:id", async (req, res) => {
   await tutorController.remove(req, res);
 });
 
