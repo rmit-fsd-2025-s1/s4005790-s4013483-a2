544cad019aec49d2cd462abfd5a17f5561d3379f
Joshua <s4013483@student.rmit.edu.au>
2025-05-05 14:24:07 +1000

    Changed lecturer routes from /lecturer to /lecturers

--- teachteam-backend/src/routes/lecturer.routes.ts
+++ teachteam-backend/src/routes/lecturer.routes.ts
@@ -4,27 +4,27 @@ import { LecturerController } from "../controller/LecturerController";
 const router = Router();
 const lecturerController = new LecturerController();
 
-router.get("/lecturer", async (req, res) => {
+router.get("/lecturers", async (req, res) => {
   await lecturerController.all(req, res);
 });
 
-router.get("/lecturer/:id", async (req, res) => {
+router.get("/lecturers/:id", async (req, res) => {
   await lecturerController.one(req, res);
 });
 
-router.get("/lecturer/:email", async (req, res) => {
+router.get("/lecturers/:email", async (req, res) => {
   await lecturerController.email(req, res);
 });
 
-router.post("/lecturer", async (req, res) => {
+router.post("/lecturers", async (req, res) => {
   await lecturerController.save(req, res);
 });
 
-router.put("/lecturer/:id", async (req, res) => {
+router.put("/lecturers/:id", async (req, res) => {
   await lecturerController.update(req, res);
 });
 
-router.delete("/lecturer/:id", async (req, res) => {
+router.delete("/lecturers/:id", async (req, res) => {
   await lecturerController.remove(req, res);
 });
 
