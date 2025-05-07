892dc8b4993cad8f3006ae7ebe2050ffdbbc984a
Joshua <s4013483@student.rmit.edu.au>
2025-05-07 17:36:10 +1000

    Added account details to lecturer profile

--- teachteam/src/pages/ProfileLecturer.tsx
+++ teachteam/src/pages/ProfileLecturer.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { useProfile } from "@/context/ProfileContext";
 import { useUser } from "@/context/UserContext";
 import { useRouter } from 'next/router';
+import { lecturerApi } from "@/services/lecturer.api";
 
 export interface ProfileLecturer {
   age: string,
@@ -21,6 +22,7 @@ export default function ProfileLecturer() {
     links: ""
   });
 
+  const [createdAt, setCreatedAt] = useState<string>("Loading...");
   const { profiles, setProfiles } = useProfile();
   const { user } = useUser();
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -28,8 +30,18 @@ export default function ProfileLecturer() {
 
   // If there is a pre-existing profile load it in.
   useEffect(() => {
+    if (user) {
+      async function fetchCreatedAt() {
+        const value = await lecturerApi.getLecturerByEmail(user.email);
+        setCreatedAt(value.createdAt);
+      }
+      fetchCreatedAt();
+    }
     if (user && profiles.has(user.email) && profiles) {
-      setProfileChange(profiles.get(user.email));
+      const profile = profiles.get(user.email);
+      if (profile) {
+        setProfileChange(profile);
+      }
     }
   }, [profiles, user]);
 
@@ -66,11 +78,24 @@ export default function ProfileLecturer() {
         <Box as="main" flex="1" w="100%" p={8} textAlign="center">
           <VStack>
             <form onSubmit={handleSubmit}>
+              <Text textAlign="center" color="#32e5b" style={{fontWeight:"bold"}}>Account Details</Text>
+              <Text textAlign="left" color="#032e5b">Name</Text>
+              <Text textAlign="left">{user?.name}</Text>
+              <Text textAlign="left" color="#032e5b">Email</Text>
+              <Text textAlign="left">{user?.email}</Text>
+              <Text textAlign="left" color="#032e5b">Created At</Text>
+              <Text textAlign="left">{createdAt}</Text>
               <FormControl>
-                <FormLabel color="#032e5b">Name</FormLabel>
-                <Input name="name" value={user?.name} readOnly />
-                <FormLabel color="#032e5b">Email</FormLabel>
-                <Input name="email" value={user?.email} readOnly />
+                {/* Uncommenting below causes errors*/}
+                {/* <Text textAlign="center" color="#32e5b" style={{fontWeight:"bold"}}>Account Details</Text> */}
+                {/* <FormLabel color="#032e5b">Name</FormLabel> */}
+                {/* <Input name="name" value={user?.name} readOnly /> */}
+                {/* <FormLabel color="#032e5b">Email</FormLabel> */}
+                {/* <Input name="email" value={user?.email} readOnly /> */}
+                {/* <Text textAlign="left" color="#032e5b">Created At</Text> */}
+                {/* <Input name="createdAt" value={createdAt ?? ""} readOnly/> */}
+                {/* <Text textAlign="left">{createdAt}</Text> */}
+                <Text textAlign="center" color="#32e5b" style={{fontWeight:"bold"}}>Profile</Text>
                 <FormLabel color="#032e5b">Age</FormLabel>
                 <Input name="age" value={profileChange.age} onChange={handleChange} />
                 <FormLabel color="#032e5b">Contact</FormLabel>
