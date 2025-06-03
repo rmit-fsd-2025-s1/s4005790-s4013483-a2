dfa5c8e9563bb7954f7568970ec08f1db48e04a4
Joshua <s4013483@student.rmit.edu.au>
2025-06-03 19:43:05 +1000

    Created separate projects for admin and its backend

---
+++ admin-backend/src/data-source.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "209.38.26.237",
+  port: 3306,
+  username: "S4013483",
+  password: "4z7&j$y0*0p9#9o0N0UQg2YQ@s!!qade33lKt0q&Xaoi@Ob5l7",
+  database: "S4013483",
+  // synchronize: true will automatically create database tables based on entity definitions
+  // and update them when entity definitions change. This is useful during development
+  // but should be disabled in production to prevent accidental data loss.
+  synchronize: true,
+  logging: true,
+  entities: [],
+  migrations: [],
+  subscribers: [],
+});
